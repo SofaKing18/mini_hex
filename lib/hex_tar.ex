72a0ced6a6b47845d96104d49520553f32e27de3
Wojtek Mach <wojtek@wojtekmach.pl>
2017-07-11 22:29:35 +0200

    Revert "Temporarily uncomment broken code"
    This reverts commit 1b663a00d72134cb90a5dc1e609e76e0cf5a32b0.

--- lib/hex_tar.ex
+++ lib/hex_tar.ex
@@ -78,23 +78,20 @@ defmodule HexTar do
     end
   end
 
-  defp checksum(files, repo) do
+  defp checksum(files, _repo) do
     case Base.decode16(files['CHECKSUM'], case: :mixed) do
       {:ok, tar_checksum} ->
         meta              = files['metadata.config']
         blob              = files['VERSION'] <> meta <> files['contents.tar.gz']
 
-        meta = decode_metadata(files['metadata.config'])
-        name = meta["name"]
-        version = meta["version"]
-
-        registry_checksum = Hex.Registry.Server.checksum(repo, to_string(name), version)
+        # TODO: check registry checksum
+        # registry_checksum = Hex.Registry.Server.checksum(repo, to_string(name), version)
         checksum          = :crypto.hash(:sha256, blob)
 
         if checksum != tar_checksum,
           do: Mix.raise "Checksum mismatch in tarball"
-        if checksum != registry_checksum,
-          do: Mix.raise "Checksum mismatch against registry"
+        # if checksum != registry_checksum,
+        #   do: Mix.raise "Checksum mismatch against registry"
 
       :error ->
         Mix.raise "Checksum invalid"
